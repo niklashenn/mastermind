f366cca13a62f45facb0cb281f34bf19fcda475b
niklashenn <41907448+niklashenn@users.noreply.github.com>
2021-06-01 13:03:21 +0200

    Update start.py

--- start.py
+++ start.py
@@ -2,10 +2,14 @@
 # imports
 from tkinter.ttk import * 
 from tkinter import *
+import sys
+
 
 class Start_window:
     windowid=0
-
+    color1= 'white'
+    color2 = 'white'
+    
     def __init__(self):
         # create frame
         
@@ -21,10 +25,12 @@ class Start_window:
         def changewindow(id):
             clear_frame()
             if(id==1):
-                window1()
+                windowgame()
+            if(id==0):
+                windowmain()
         
         ##########################################################################################################################################################################################
-        def window0():
+        def windowmain():
             self.geometry("400x700") # set frame size
             # set weight of rows and columns
             self.grid_rowconfigure(8, weight=1)
@@ -75,13 +81,87 @@ class Start_window:
             self.play_bt.grid(row = 8, column = 0, pady = 5, sticky='s')
 
         ##########################################################################################################################################################################################
-        def window1():
-            self.gamemode_lb = Label(self, text = "Gamemode", width = 20, font=("Fixedsys", 17))
-            self.gamemode_lb.grid(row = 1, column = 0, padx=5, sticky='nwse')
-            self.geometry("800x700")
         
+        def windowgame():
+            self.geometry("600x1250")
+            self.title("Mastermind")
 
-        window0()
+            self.title_lb = Label(self, text = "Mastermind", width = 20, font=("Fixedsys", 40))
+            self.title_lb.grid(row = 0, column = 0, pady = 10, sticky='nwse')
+            canvas()
+            
+        def canvas():
+            canvaswidth = 455
+            canvasheight = 90
+            canvaspadx = 50
+            canvaspady = 4
+            canvasbg = "white"
+            canvashbg = "black"
+            canvasht = 2
+            
+            canvas1 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas1.grid(row=1, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas1.create_oval(10, 10, 50, 50, fill=Start_window.color1 , outline='black', tag="oval1")
+            canvas1.tag_bind("oval1", "<Button-1>", lambda x: save_posn(1))
+            
+            canvas1.create_oval(60, 10, 100, 50, fill=Start_window.color2 , outline='black', tag="oval2")
+            canvas1.tag_bind("oval2", "<Button-1>", lambda x: save_posn(2))
+            
+            #canvas1.bind("<Button-1>", save_posn)
+    
+            canvas2 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas2.grid(row=2, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas3 = Canvas(self, width=canvaswidth, height=canvasheight,  highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas3.grid(row=3, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas4 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas4.grid(row=4, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas5 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas5.grid(row=5, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas6 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas6.grid(row=6, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas7 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas7.grid(row=7, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas8 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas8.grid(row=8, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas9 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas9.grid(row=9, column=0, padx=canvaspadx, pady=canvaspady)
+
+            canvas10 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas10.grid(row=10, column=0, padx=canvaspadx, pady=canvaspady)
+            
+            canvas11 = Canvas(self, width=canvaswidth, height=canvasheight, highlightthickness=canvasht, bg=canvasbg, highlightbackground=canvashbg)
+            canvas11.grid(row=11, column=0, padx=canvaspadx, pady=canvaspady*5)
+
+        
+            
+        def save_posn(x):    
+            popup()
+            if(x==1):
+                Start_window.color1 = 'black'
+            if(x==2):
+                Start_window.color2 = 'black'
+            canvas()
+        
+        def popup():
+            self = Tk()
+            self.title("Mastermind - Start") # set frame title
+            self.iconbitmap('img/icon.ico') # set frame icon
+            self.geometry("50x50")
+
+            #newWindow = Toplevel(self)
+            #labelExample = Label(self.newWindow, text = "New Window")
+            #buttonExample = Button(self.newWindow, text = "New Window button")
+
+
+        windowgame()
         
         # show everything
         
@@ -90,175 +170,3 @@ class Start_window:
         
 # create new start window
 start = Start_window()
-
-"""import tkinter as tk
-from tkinter.constants import CURRENT
-from tkinter.ttk import * 
-import random
-
-
-LARGE_FONT= ("Verdana", 12)
-
-class SeaofBTCapp(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-
-        container.pack(side="top", fill="both", expand = True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-
-        for F in (StartPage, PageOne, PageTwo, PageThree):
-
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(StartPage)
-
-    def show_frame(self, cont):
-        container = tk.Frame(self)
-
-        container.pack(side="top", fill="both", expand = True)
-
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-        for F in (StartPage, PageOne, PageTwo, PageThree):
-
-            frame = F(container, self)
-
-            self.frames[F] = frame
-
-            frame.grid(row=0, column=0, sticky="nsew")
-        
-        frame = self.frames[cont]
-
-        
-class StartPage(tk.Frame):
-    
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self,parent)
-        
-        # create title label
-        title_lb = tk.Label(self, text = "Mastermind", width = 20, font=("Fixedsys", 40))
-        title_lb.grid(row = 0, column = 0, pady = 10, sticky='nwse')
-
-        # create gameart label
-        gamemode_lb = tk.Label(self, text = "Gamemode", width = 20, font=("Fixedsys", 17))
-        gamemode_lb.grid(row = 1, column = 0, padx=5, sticky='nwse')
-
-        # create radio button for PvC
-        var = tk.StringVar()
-        player_vs_computer_rb = tk.Radiobutton(self, text="Player vs Computer", padx = 50, variable=var, value="PvC")
-        player_vs_computer_rb.grid(row = 2, column = 0, padx=5, sticky='nwse')
-        player_vs_computer_rb.deselect()
-
-        # create radio button for PvP
-        player_vs_player_rb = tk.Radiobutton(self, text="Player vs Player", padx = 50, variable=var, value="PvP")
-        player_vs_player_rb.grid(row = 3, column = 0, padx=5, sticky='nwse')
-        player_vs_player_rb.deselect()
-
-        # create radio button for CvP
-        computer_vs_player_rb = tk.Radiobutton(self, text="Computer vs Player", padx = 50, variable=var, value="CvP")
-        computer_vs_player_rb.grid(row = 4, column = 0, padx=5, sticky='nwse')
-        computer_vs_player_rb.deselect()
-
-        # create options label
-        options_lb = tk.Label(self, text = "", width = 1, font=("Fixedsys", 17))
-        options_lb.grid(row = 5, column = 0, padx=1, sticky='nwse',pady=50)
-
-        # create options label
-        options_lb = tk.Label(self, text = "Options", width = 1, font=("Fixedsys", 17))
-        options_lb.grid(row = 6, column = 0, padx=1, sticky='nwse')
-        
-        # create optionmenu for moves
-        moves_cb = Combobox(self, values=['1', '2', '3', '4', '5', '6'])
-        moves_cb.current(0)
-        moves_cb.grid(row=7, column=0, padx=5, pady = 5)
-
-        # create exit button
-        exit_bt = tk.Button(self, text = "Exit", width = 6)
-        #self.exit_bt.pack(side=RIGHT)
-        
-        btpvp = tk.Button(self, text = "Play", font=("Fixedsys", 14), width = 15, height=5, bg="lightgreen", command=lambda: controller.show_frame(PageOne))
-        btpvc = tk.Button(self, text = "Play", font=("Fixedsys", 14), width = 15, height=5, bg="lightgreen", command=lambda: controller.show_frame(PageTwo))
-        btcvp = tk.Button(self, text = "Play", font=("Fixedsys", 14), width = 15, height=5, bg="lightgreen", command=lambda: controller.show_frame(PageThree))
-        # create play button
-        def choice():
-            print(moves_cb.get())
-            if(var.get()=="PvP"):
-                Helper.testi =moves_cb.get()
-                btpvp.invoke()
-                PageOne.update(self)
-                print(Helper.testi)
-            elif(var.get()=="PvC"):
-                vari=moves_cb.get()
-                btpvc.invoke()
-            elif(var.get()=="CvP"):
-                vari=moves_cb.get()
-                btcvp.invoke()
-        
-        #def moves_cb_value():
-            #return moves_cb.get()
-
-        play_bt = tk.Button(self, text = "Play", font=("Fixedsys", 14), width = 15, height=5, bg="lightgreen", command=choice)
-        play_bt.grid(row = 8, column = 0, pady = 5, sticky='s')
-
-
-class PageOne(tk.Frame):
-    
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        
-        title_lb = tk.Label(self, text = Helper.testi, width = 20, font=("Fixedsys", 40))
-        title_lb.grid(row = 0, column = 0, pady = 10, sticky='nwse')
-
-        button1 = tk.Button(self, text="Back to Home",command=lambda: controller.show_frame(StartPage))
-        button1.grid(row = 1, column = 0, pady = 10, sticky='nwse')
-
-        button2 = tk.Button(self, text="Page Two", command=lambda: controller.show_frame(PageTwo))
-        button2.grid(row = 2, column = 0, pady = 10, sticky='nwse')
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        title_lb = tk.Label(self, text = "Test", width = 20, font=("Fixedsys", 40))
-        title_lb.grid(row = 0, column = 0, pady = 10, sticky='nwse')
-
-        button1 = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
-        button1.grid(row = 1, column = 0, pady = 10, sticky='nwse')
-
-        button2 = tk.Button(self, text="Page One", command=lambda: controller.show_frame(PageOne))
-        button2.grid(row = 2, column = 0, pady = 10, sticky='nwse')
-
-
-class PageThree(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-
-        title_lb = tk.Label(self, text = "Page Three", width = 20, font=("Fixedsys", 40))
-        title_lb.grid(row = 0, column = 0, pady = 10, sticky='nwse')
-
-        button1 = tk.Button(self, text="Back to Home", command=lambda: controller.show_frame(StartPage))
-        button1.grid(row = 1, column = 0, pady = 10, sticky='nwse')
-
-        button2 = tk.Button(self, text="Page One", command=lambda: controller.show_frame(PageOne))
-        button2.grid(row = 2, column = 0, pady = 10, sticky='nwse')
-        
-class Helper():
-    testi = 1
-
-
-app = SeaofBTCapp()
-app.mainloop()"""
